e1134260b74b9648d72a98fd727dd58dec6a9fb5
leeonky <leeonky@gmail.com>
2017-10-29 15:16:23 +0000

    rigist exit signal

--- mem/circular_shm.cc
+++ mem/circular_shm.cc
@@ -1,9 +1,11 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <cstring>
+#include <signal.h>
 #include "circular_shm.h"
 #include "stdexd/stdexd.h"
 #include "sysexd/sysexd.h"
+#include "sysport/sysport.h"
 #include "mem/circular_shm.h"
 
 namespace {
@@ -11,18 +13,37 @@ namespace {
 		size_t page_size = getpagesize();
 		return (size+page_size-1)/page_size*page_size;
 	}
+
+	int last_shm_id = -1, last_sem_id = -1;
+	static void clean_last_cbuf(int s) {
+		if(-1 != last_shm_id)
+			shmctl(last_shm_id, IPC_RMID, NULL);
+		if(-1 != last_sem_id)
+			sem_unlink_with_id(last_sem_id);
+		fprintf(stderr, "abort(%d)\n", s);
+		exit(-1);
+	}
+
+	void register_cbuf_clean() {
+		signal(SIGINT, clean_last_cbuf);
+		signal(SIGHUP, clean_last_cbuf);
+		signal(SIGKILL, clean_last_cbuf);
+		signal(SIGPIPE, clean_last_cbuf);
+		signal(SIGTERM, clean_last_cbuf);
+	}
 }
 
 int circular_shm::initialize_and_action(size_t size, int count, const std::function<int(circular_shm &)> &action) {
 	element_size = alignment_size(size);
 	element_count = count;
 	return shmget(element_size*count, [this, &action](int id) {
-			shm_id = id;
+			last_shm_id = shm_id = id;
 			return shmat(id, [this, &action](void *buf) {
 				buffer = static_cast<int8_t *>(buf);
-				sem_id = getpid();
+				last_sem_id = sem_id = getpid();
 				return sem_new_with_id(sem_id, element_count, [this, &action](sem_t *s){
 					semaphore = s;
+					register_cbuf_clean();
 					return action(*this);
 					});
 				});
