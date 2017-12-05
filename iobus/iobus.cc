d8f05e853b487ffcbf5f8309288671a34bdd22e3
leeonky <leeonky@gmail.com>
2017-12-05 15:10:15 +0000

    screen for show video

--- iobus/iobus.cc
+++ iobus/iobus.cc
@@ -7,7 +7,6 @@
 #define log_error(format, ...) log_error("libiobus", (format), ## __VA_ARGS__)
 
 int iobus::get(const std::function<int(const char *, const char *)> &action) {
-	int res = 0;
 	size_t len = 0;
 	if(processed) {
 		if(-1 != getline(&line, &len, file_in) && 1 == sscanf(line, "%s", command)) {
@@ -17,9 +16,7 @@ int iobus::get(const std::function<int(const char *, const char *)> &action) {
 		} else
 			return -1;
 	}
-	if(!(res = action(command, arguments)))
-		processed = true;
-	return res;
+	return action(command, arguments);
 }
 
 int iobus::get(const char *event, const std::function<int(const char *, const char *)> &action, int arg_count, const char *format, ...) {
@@ -29,6 +26,7 @@ int iobus::get(const char *event, const std::function<int(const char *, const ch
 	res = get([&](const char *command, const char *arguments) {
 			int r = 0;
 			if(!strcmp(command, event)) {
+				processed = true;
 				if(arg_count == vsscanf(arguments, format, args)) {
 					r = action(command, arguments);
 				} else
---
+++ screen/main.cc
@@ -0,0 +1,39 @@
+#include "stdexd/stdexd.h"
+#include "lffmpeg/lffmpeg.h"
+#include "mem/circular_shm.h"
+#include "iobus/iobus.h"
+#include "lsdl2/lsdl2.h"
+#include "media/media.h"
+
+int play_main() {
+	iobus iob(stdin, stdout, stderr);
+	return video_event(iob, [&](int fw, int fh, enum AVPixelFormat av_format){
+			return SDL_CreateWindow("", 0, 0, 1920, 1080, 0,
+				[&](SDL_Window *window){
+				return SDL_CreateTexture(window, fw, fh, AVPixelFormat_to_SDL(av_format),
+					[&](int, int, SDL_Renderer *renderer, SDL_Texture *texture){
+					return buffer_event(iob, [&](int shmid, size_t size, int count, int semid) {
+						return circular_shm::load(shmid, size, count, semid,
+							[&](circular_shm &shm){
+							while(!frames_event(iob, [&](frame_list &frames){
+									for(int i=0; i<frames.count; i++){
+										shm.free(frames.frames[i].index, [&](void *buffer){
+											return av_image_fill_arrays(fw, fh, av_format, buffer, [&](uint8_t **datas, int *lines){
+												return SDL_PresentYUV(renderer, texture, datas, lines);
+												});
+											});
+									}
+									return 0;
+									}))
+							;
+							return 0;
+							});
+						});
+					});	
+				});
+			});
+}
+
+int main(int, char **) {
+	play_main();
+}
