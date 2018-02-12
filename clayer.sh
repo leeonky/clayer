80a465001c1d4dfe10a16b44971d77dc92178380
leeonky <leeonky@gmail.com>
2018-02-12 06:11:52 +0000

    refine clayer.sh

--- clayer.sh
+++ clayer.sh
@@ -2,8 +2,19 @@
 # clayer file [v=index] [a=index] [sub=subtitle_file]
 
 project_path="$(dirname "$0")"
-
 media_file=$1
 
-"$project_path/decoder/decoder" "$media_file" | "$project_path/screen/screen" &
-"$project_path/decoder/decoder" "$media_file" -a 0 | "$project_path/resampler/resampler" -f pack:flt32:maxbit32 -lmax5.1 | "$project_path/speaker/speaker"
+shift 1
+for args in "$@"
+do
+        eval "$args"
+done
+
+video=${v:-0}
+audio=${a:-0}
+subtitle=${sub:-}
+
+(
+	"$project_path/decoder/decoder" "$media_file" -v $video &
+	"$project_path/decoder/decoder" "$media_file" -a $audio | "$project_path/resampler/resampler" -f pack:flt32:maxbit32 -lmax5.1 | "$project_path/speaker/speaker"
+) | "$project_path/screen/screen"
