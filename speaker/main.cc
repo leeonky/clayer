f6ca3fc0f80a17b826adaed98cfef287edf2c945
leeonky <leeonky@gmail.com>
2018-03-04 04:16:39 +0000

    use port audio in speaker

--- speaker/main.cc
+++ speaker/main.cc
@@ -3,6 +3,7 @@
 #include "mem/circular_shm.h"
 #include "iobus/iobus.h"
 #include "lsdl2/lsdl2.h"
+#include "lport_audio/lport_audio.h"
 #include "media/media.h"
 
 static int play_with_sdl2(iobus &iob) {
@@ -32,8 +33,30 @@ static int play_with_sdl2(iobus &iob) {
 			});
 }
 
+static int play_with_portaudio(iobus &iob) {
+	return audio_event(iob, [&](int sample_rate, int channels, int64_t /*layout*/, enum AVSampleFormat format){
+			return buffer_event(iob, [&](int shmid, size_t size, int count, int semid, int /*audio_buffer_key*/) {
+				return circular_shm::load(shmid, size, count, semid, [&](circular_shm &shm){
+					return Pa_Init_OpenOutputStream(0, sample_rate, channels, AVSampleFormat_to_PortAudio(format), [&](PaStream *stream){
+						while (!samples_event(iob, [&](sample_list &samples) {
+								//if(samples.count)
+									//iob.post("CLOCK base:%" PRId64 " offset:%" PRId64, usectime(), samples.samples[0].timestamp-SDL_AudioLast(device_id, audio_spec));
+								for(int i=0; i<samples.count; i++)
+									shm.free(samples.samples[i].index, [&](void *buffer){
+										Pa_WriteStream(stream, buffer, samples.samples[i].nb_samples);
+										});
+								return 0;
+								}))
+						;
+						return 0;
+						});
+					});
+				});
+			});
+}
+
 int main(int, char **) {
 	iobus iob(stdin, stdout, stderr);
-	return play_with_sdl2(iob);
+	return play_with_portaudio(iob);
 }
 
